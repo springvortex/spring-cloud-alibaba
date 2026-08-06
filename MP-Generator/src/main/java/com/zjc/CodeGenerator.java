482c816b473deb5fc21c4b8d2f295f2f4560413e
jiancai.zhong <jiancai.zhong.1997@gmail.com>
2026-08-06 15:18:16 +0800

    refactor(generator): 优化代码生成器配置
    - 修改输出目录为动态获取的模块根目录路径
    - 将包名从 com.zjc 修改为 com.zjc.provider
    - 将数据访问层包名从 dao 修改为 mapper
    - 将Mapper文件后缀从 DAO 修改为 Mapper
    - 添加异常处理声明
    - 引入File类用于目录操作

--- MP-Generator/src/main/java/com/zjc/CodeGenerator.java
+++ MP-Generator/src/main/java/com/zjc/CodeGenerator.java
@@ -4,18 +4,24 @@ import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import com.baomidou.mybatisplus.generator.model.ClassAnnotationAttributes;
 
+import java.io.File;
+
 public class CodeGenerator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        File moduleRoot = new File(CodeGenerator.class.getProtectionDomain()
+                .getCodeSource().getLocation().toURI()).getParentFile().getParentFile();
+        String outputDir = moduleRoot.toPath().resolve("src/main/java").toString();
+
         FastAutoGenerator.create("jdbc:mysql://192.168.100.128:3306/spring_cloud_alibaba?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&allowMultiQueries=true",
                         "spring_cloud_alibaba",
                         "fEmTS3Cfz3DTpMMS")
                 .globalConfig(builder -> builder.author("jiancai.zhong")
                         .commentDate("yyyy-MM-dd")
-                        .outputDir("src/main/java")
+                        .outputDir(outputDir)
                         .disableOpenDir())
-                .packageConfig(builder -> builder.parent("com.zjc")
+                .packageConfig(builder -> builder.parent("com.zjc.provider")
                         .entity("entity")
-                        .mapper("dao")
+                        .mapper("mapper")
                         .service("service")
                         .serviceImpl("service.impl")
                         .xml("mapper"))
@@ -25,7 +31,7 @@ public class CodeGenerator {
                         .enableLombok(new ClassAnnotationAttributes("@Data", "lombok.Data"))
                         .enableTableFieldAnnotation()
                         .mapperBuilder()
-                        .convertMapperFileName((entityName -> entityName + "DAO"))
+                        .convertMapperFileName((entityName -> entityName + "Mapper"))
                         .enableBaseResultMap()
                         .enableBaseColumnList()
                         // 设置service接口
