3889e425f12a75f36d94f2dc97b697f359d579d3
zjc <jiancai.zhong.1997@gmail.com>
2026-08-25 16:27:53 +0800

    drop(database): 删除分布式锁表结构
    - 移除 t_distributed_lock 表的创建语句
    - 删除锁 key、持有者标识、重入次数和到期时间字段定义
    - 移除表的主键约束设置
    - 清理 InnoDB 引擎和 UTF8MB4 字符集配置

--- service-provider/src/main/resources/sql/t_distributed_lock.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS t_distributed_lock (
-    lock_key VARCHAR(191) NOT NULL COMMENT '锁 key',
-    lock_owner VARCHAR(120) NOT NULL COMMENT '持有者标识',
-    lock_count INT NOT NULL COMMENT '重入次数',
-    expire_at DATETIME(3) NOT NULL COMMENT '租约到期时间',
-    PRIMARY KEY (lock_key)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4 COMMENT = 'MySQL 分布式锁租约表';
